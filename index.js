2356d098043285ef2a1121ace1941f4b4a4a7ef3
nickgsu <96020805+nickgsu@users.noreply.github.com>
2022-03-11 18:14:25 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,6 @@
+   if (bandera === 1)
+    {
+
+        grupo.animate({transform: 's1,300,100' }, 2000, mina.easeinout())
+        negro.animate({opacity: '1'}, 1000, mina.easeinout())
+        suarez.animate({opacity: '1'}, 1000, mina.easeinout())
